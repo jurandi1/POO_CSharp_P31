da973047e65a2376037e0d19cbf665f9c66646ee
Jurandi Junior <jurandijr20@gmail.com>
2025-11-13 21:12:59 -0300

    Func (System)

---
+++ Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace POO_CSharp_P31.Entities
+{
+    internal class Product
+    {
+        public Product()
+        {
+        }
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString() { 
+        return Name + ", " + Price.ToString("F2");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,29 @@
-﻿internal class Program
+﻿using POO_CSharp_P31.Entities;
+using System.Linq;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        List<Product> list = new List<Product>();
+        list.Add(new Product("TV", 900.00));
+        list.Add(new Product("Notebook", 1200.00));
+        list.Add(new Product("Mouse", 50.00));
+        list.Add(new Product("Tablet", 450.00));
+        list.Add(new Product("HD CASE", 80.90));
+
+        //Fun<Product, string> func = NameUpper; ou //p.Name.ToUpper();
+
+        List<string> result = list.Select(p => p.Name.ToUpper()).ToList();
+        foreach (string s in result)
+        {
+            Console.WriteLine(s);
+        }
+
     }
+
+    //static string NameUpper(Product p)
+    //{
+    //    return p.Name = p.Name.ToUpper();
+    //}
 }
